30e10316520bcf9b983f73b88ecb80f5bebc3b4f
kim-a9 <kimberlysantosalves@gmail.com>
2026-03-03 20:24:16 -0300

    config básica logs estruturados

--- todo-list/src/main/main.ts
+++ todo-list/src/main/main.ts
@@ -1,11 +1,16 @@
 import express from 'express';
 import cors from 'cors';
+import { logger } from '../infrastructure/database/mongodb/logger/pino.logger';
 
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+    logger.info({message: 'Incoming Request', method: req.method, url: req.url}),
+    next();
+});
 
 
 
