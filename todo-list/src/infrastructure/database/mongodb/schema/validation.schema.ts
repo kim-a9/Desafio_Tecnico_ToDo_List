87957d62abdb6b130eb6e8e80921510aff050224
kim-a9 <kimberlysantosalves@gmail.com>
2026-03-04 21:18:07 -0300

    feat: endpoint PUT /tasks

---
+++ todo-list/src/infrastructure/database/mongodb/schema/validation.schema.ts
@@ -0,0 +1,9 @@
+import { z } from 'zod';
+
+export const validationSchema = z.object({
+    title: z.string().min(3).max(50),
+    description: z.string().min(3).max(50),
+    status: z.enum(['pending', 'in_progress', 'completed', 'cancelled']),
+    priority: z.enum(['low', 'medium', 'high']),
+    due_date: z.string().pipe(z.coerce.date()).optional()
+})
