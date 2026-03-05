ec479a71dddb4b164688458219bc262be3a13f91
kim-a9 <kimberlysantosalves@gmail.com>
2026-03-05 14:22:05 -0300

    fix: método update

--- todo-list/src/application/usecases/update.task.usecase.ts
+++ todo-list/src/application/usecases/update.task.usecase.ts
@@ -16,6 +16,15 @@ export class UpdateTaskUsecase {
         if (task.status === 'completed') {
             throw new Error("Tarefas completadas não podem ser editadas.")
         }
+        //verifico SE usuário está atualizando o campo due_date
+        if (data.due_date) {
+            const today = new Date();
+            today.setHours(0,0,0,0);
+            
+            if(data.due_date < today) {
+                throw new Error("A nova data de vencimento não pode ser anterior a de hoje.")
+            }
+        }
 
         const updatedTask = new Task({
             ...task.toJSON(),
