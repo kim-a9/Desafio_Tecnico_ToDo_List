c3a7ff9701ab799d0617fb41f0ae1ee6b1e5804a
kim-a9 <kimberlysantosalves@gmail.com>
2026-03-05 19:58:00 -0300

    test: create e get (all, by id, stats e priority)

---
+++ todo-list/tests/unit/create.task.spec.ts
@@ -0,0 +1,43 @@
+import { CreateTaskUsecase } from "../../src/application/usecases/create.task.usecase";
+import { Task } from "../../src/domain/entities/task";
+import { TaskStatus } from "../../src/domain/entities/task";
+import { TaskPriority } from "../../src/domain/entities/task";
+
+describe("Create Task Unit Test", () => {
+    const taskData = {
+            id: "12345",
+            title: "Task",
+            description: "Testar com Jest",
+            status: 'pending' as TaskStatus,
+            priority: 'medium' as TaskPriority,
+            due_date: new Date(),
+            created_at: new Date(),
+            updated_at: new Date(),
+        };
+
+    const mockRepo = {
+        create: jest.fn().mockResolvedValue(taskData),
+    }
+
+    beforeAll(() => {
+        jest.clearAllMocks();
+    });
+
+
+    it("Deve criar nova task com sucesso", async () => {
+        const createTask = new CreateTaskUsecase(mockRepo as any);
+        const r = await createTask.execute(taskData);
+
+        expect(mockRepo.create).toHaveBeenCalled();
+        expect(r).toHaveProperty("title", taskData.title);
+
+    });
+
+
+
+
+
+
+
+
+})
---
+++ todo-list/tests/unit/get.task.spec.ts
@@ -0,0 +1,98 @@
+import { Task, TaskPriority, TaskStatus } from "../../src/domain/entities/task";
+import { GetTaskByIdUsecase, GetTasksUseCase } from "../../src/application/usecases/get.task";
+
+
+describe("Get Task Unit Test", () => {
+    const taskData = {
+        id: "12345",
+        title: "Task",
+        description: "Testar com Jest",
+        status: 'pending' as TaskStatus,
+        priority: 'medium' as TaskPriority,
+        due_date: new Date(),
+        created_at: new Date(),
+        updated_at: new Date(),
+    }; 
+
+    const taskInstance = new Task(taskData);
+
+    const mockRepo = {
+        create: jest.fn(),
+        findById: jest.fn().mockResolvedValue(taskInstance),
+        findAll: jest.fn().mockRejectedValue(taskInstance),
+
+    }
+
+    beforeAll(() => {
+        jest.clearAllMocks();
+    });
+
+
+    it("Deve buscar task pelo id com sucesso", async () => {
+        const getTaskId = new GetTaskByIdUsecase(mockRepo as any);
+        const task = await getTaskId.execute("12345");
+
+        expect(mockRepo.findById).toHaveBeenCalledWith("12345");
+        expect(task).toHaveProperty("id", "12345");
+        expect(task).toHaveProperty("title", "Task");
+
+
+    })
+
+
+    it("Deve retornar erro ao não encontrar task", async () => {
+        mockRepo.findById.mockResolvedValue(null);
+        const getTaskId = new GetTaskByIdUsecase(mockRepo as any);
+
+
+        await expect(getTaskId.execute("999")).rejects.toThrow("Não foi possível encontrar a Task");
+
+    });
+
+    it("Deve buscar todas as tasks com sucesso", async () => {
+        const taskData2 ={
+        id: "6789", 
+        title: "Task example",
+        description: "Testar com Jest",
+        status: 'pending' as TaskStatus,
+        priority: 'medium' as TaskPriority,
+        due_date: new Date(),
+        created_at: new Date(),
+        updated_at: new Date(),
+        };
+
+        const taskInstance = new Task(taskData2)
+        mockRepo.findAll.mockResolvedValue([taskInstance]);
+
+        const allTasks = new GetTasksUseCase(mockRepo as any);
+        const tasks = await allTasks.execute({});
+
+        expect(tasks).toBeInstanceOf(Array);
+        expect(mockRepo.findAll).toHaveBeenCalledWith({});
+    });
+
+    it("Deve filtrar busca por status", async () => {
+        mockRepo.findAll.mockResolvedValue([taskInstance]);
+        const taskStatus = new GetTasksUseCase(mockRepo as any);
+        const tasks = await taskStatus.execute({ status: 'pending'});
+
+        expect(mockRepo.findAll).toHaveBeenCalledWith({ status: 'pending'});
+
+    })
+
+    it("Deve filtrar busca por prioridade", async () => {
+        mockRepo.findAll.mockResolvedValue([taskInstance]);
+        const taskPriority = new GetTasksUseCase(mockRepo as any);
+        const tasks = await taskPriority.execute({ priority: 'high'});
+
+        expect(mockRepo.findAll).toHaveBeenCalledWith({ priority: 'high'});
+
+    })
+
+
+
+
+
+
+
+});
