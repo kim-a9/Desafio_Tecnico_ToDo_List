999c6f6be74485f49f9501a0417794ff49713d25
kim-a9 <kimberlysantosalves@gmail.com>
2026-03-04 22:27:02 -0300

    fix: configuração cors

--- todo-list/src/main/main.ts
+++ todo-list/src/main/main.ts
@@ -8,7 +8,11 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
 app.use(express.json());
 app.use(router);
 app.use((req, res, next) => {
